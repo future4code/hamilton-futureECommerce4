33bb2c234c7a87e1b87f4b19fc0963471a5c12d4
igordelesposti <igordelesposti@gmail.com>
2020-03-25 14:46:59 -0300

    Criação layout future-commerce-4

---
+++ future-commerce-4/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
+import CarrinhoCompra from './Components/CarrinhoCompra';
+import Display from './Components/Display';
+import Footer from './Components/Footer';
+
+
+
+const Container = styled.div`
+  
+`
+
+function App() {
+  return (
+    <Container>
+          <Header />
+          <NavBar />
+          <CarrinhoCompra />
+          <Display />
+          <Footer />
+
+    </Container>
+  );
+}
+
+export default App;
